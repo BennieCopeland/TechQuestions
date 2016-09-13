aed88f39408c0fd2edc98caaa4e02a93023789c0
Bennie Copeland <mugen.kanosei@gmail.com>
2016-09-13 14:56:10 +0900

    Question 3 answered

---
+++ Question 3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // no way to actually set the radius
+            Circle circle = new Circle();
+
+            // using a lambda expression
+            double answer = circle.Calculate(radius =>
+                {
+                    return 2 * Math.PI * radius;
+                });
+
+            // using a delegate
+            answer = circle.Calculate(delegate(double radius)
+            {
+                return 2 * Math.PI * radius;
+            });
+
+            // using a method
+            answer = circle.Calculate(Circumference);
+
+            Console.WriteLine(answer);
+        }
+
+        static double Circumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+    public sealed class Circle
+    {
+        private double radius;
+
+        public double Calculate(Func<double, double> op)
+        {
+            return op(radius);
+        }
+    }
+
+}
